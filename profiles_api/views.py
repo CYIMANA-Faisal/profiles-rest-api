7dd18bc3bbdb454447fdd2a5f4760d30e8945c86
CYIMANA-Faisal <cyimanafaisal@gmail.com>
2020-01-13 16:00:50 +0200

    Added hello api view and urls configurations from root project to an profiles_api app

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """ test api view """
+
+    def get(self, request, format=None):
+        """Return a list of APIview features"""
+        an_apiview = [
+            'Uses standard HTTP methods as functions (get, post, put, patch, delete)',
+            'simillar to django standard views',
+            'Gives you mosta conctrol over your application logic',
+            'Is mapped to URl manually',
+        ]
+        return Response({'message':'Hello' , 'an_apiview':an_apiview})
